eeea4244255a6eb19f0816322c49262619f30645
jorge Luis Pena Real <falmemor@gmail.com>
2022-01-21 19:45:34 -0600

    added map pokemons

--- src/app/pokemon/shared/services/pokemons.service.ts
+++ src/app/pokemon/shared/services/pokemons.service.ts
@@ -1,3 +1,4 @@
+import { Result, SimplePokemon } from "./../interfaces/pokemonInterfaces";
 import { PokemonPaginatedResponse } from "../interfaces/pokemonInterfaces";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
@@ -9,6 +10,13 @@ import { HttpClient } from "@angular/common/http";
 export class PokemonsService {
   pokemonApi = "https://pokeapi.co/api/v2/pokemon?limit=40";
 
+  simplePokemonList: SimplePokemon[] = [];
+  set setSimplePokemonList(pokemons: SimplePokemon[]) {}
+  loading: boolean = false;
+  set setIsLoading(val: boolean) {
+    this.loading = val;
+  }
+
   constructor(private http: HttpClient) {}
 
   get newPokemonsPage(): Observable<PokemonPaginatedResponse> {
@@ -17,4 +25,19 @@ export class PokemonsService {
   set snewPokemonApiPaginated(url: string) {
     this.pokemonApi = url;
   }
+  loadPokemons = async () => {
+    this.setIsLoading = true;
+  };
+  mapPokemonList = (pokemonList: Result[]) => {
+    const newPokemonList: SimplePokemon[] = pokemonList.map(({ name, url }) => {
+      const urlParts = url.split("/");
+      const id = urlParts[urlParts.length - 2];
+      const picture = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`;
+
+      return { id, picture, name };
+    });
+
+    this.setSimplePokemonList = [...this.simplePokemonList, ...newPokemonList];
+    this.setIsLoading = false;
+  };
 }
